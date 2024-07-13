2b4030d1adaa3b80d498ca99ec4ed1fdc217e4f8
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-07-13 16:01:20 +0300

    Add lab10 project

---
+++ Lab10/Task1/Angle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Angle
+    {
+        private int gradus;
+        private int min;
+        private int sec;
+
+        public int Gradus
+        {
+            get => gradus;
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get => min;
+            set
+            {
+                if (value < 0)
+                {
+                    min = 0; 
+                }
+                else if (value > 59)
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+                else
+                {
+                    min = value;
+                }
+            }
+        }
+        public int Sec
+        {
+            get => sec;
+            set
+            {
+                if (value < 0)
+                {
+                    sec = 0;
+                }
+                else if (value > 59)
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+                else
+                {
+                    sec = value;
+                }
+            }
+        }
+
+        public Angle()
+        : this(0, 0, 0)
+        { }
+
+        public Angle(int gradus, int min = 0, int sec = 0)
+        {
+            Gradus = gradus; Min = min; Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            double gradus = (Min + Sec / 60.0) / 60.0;
+            if (Gradus < 0)
+            {
+                gradus = -1 * gradus;
+            }
+            return (Gradus + gradus) * Math.PI / 180;
+        }
+    }
+}
---
+++ Lab10/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle A = new Angle(90, 120, 0);
+            A.Sec = 72;
+            Console.WriteLine("Gradus={0}, Min={1}, Sec={2}", A.Gradus, A.Min, A.Sec);
+            Console.WriteLine(A.ToRadians());
+        }
+    }
+}
