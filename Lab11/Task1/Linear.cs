743f681838e7be117742f2557484c19c0bd6236f
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-07-13 18:19:15 +0300

    Add lab11 project

---
+++ Lab11/Task1/Linear.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal struct Linear
+    {
+        /* Разработать структуру для решения линейного уравнения 0=kx+b */
+        /* Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. */
+
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            string result = String.Format("При k={0} и b={1} уравнение 0=kx+b", k, b);
+            if (k == 0) {
+                if (b == 0)
+                {
+                    result += " имеет множество решений.";
+                }
+                else
+                {
+                    result += " не имеет решений.";
+                }
+            }
+            else
+            {
+                double x = -b / k;
+                result += $" имеет решение x={x}.";
+            }
+            return result;
+        }
+    }
+}
---
+++ Lab11/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Linear linear1 = new Linear(0, 0);
+            Console.WriteLine(linear1.Root());
+
+            Linear linear2 = new Linear(0, 1);
+            Console.WriteLine(linear2.Root());
+
+            Linear linear3 = new Linear(1, 0);
+            Console.WriteLine(linear3.Root());
+
+            Linear linear4 = new Linear(1, 2);
+            Console.WriteLine(linear4.Root());
+
+            Console.ReadKey();
+        }
+    }
+}
