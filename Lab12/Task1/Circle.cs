35918f416cfa643dde2a38205e6e5ffeea61e399
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-07-14 13:06:09 +0300

    Add lab12 project

---
+++ Lab12/Task1/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal static class Circle
+    {
+        public static double Length(uint radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double Area(uint radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static bool IsPointWithinCirlce(double centerX, double centerY, uint radius, double x, double y)
+        {
+            return Math.Pow((x - centerX), 2) + Math.Pow((y - centerY), 2) <= radius * radius;
+        }
+    }
+}
---
+++ Lab12/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Длина окружности: {0:0.000}", Circle.Length(1));
+            Console.WriteLine("Площадь круга: {0:0.000}", Circle.Area(2));
+
+            if (Circle.IsPointWithinCirlce(0, 0, 1, 0.5, 0.5))
+            {
+                Console.WriteLine("Точка внутри круга");
+            }
+            else
+            {
+                Console.WriteLine("Точка вне круга");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
