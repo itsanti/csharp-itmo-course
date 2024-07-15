390764cd3dd19d9c36ac26b3bceb70dd8bec978f
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-07-15 21:41:45 +0300

    Add lab18 project

---
+++ Lab18/Task1/BracketChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class BracketChecker
+    {
+        static readonly Dictionary<char, char> brackets = new Dictionary<char, char> {
+            {'(', ')'}, {'[', ']'}, {'{', '}'},
+        };
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s) {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(brackets[c]);
+                }
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    if (stack.Count == 0 || (stack.Count > 0 && stack.Peek() != c))
+                    {
+                        return false;
+                    }
+
+                    stack.Pop();
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Lab18/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ok = "he[ll]o (world) ({log} word)";
+
+            if (BracketChecker.IsValid(ok))
+            {
+                Console.WriteLine("Верная скобочная последовательность");
+            }
+            else
+            {
+                Console.WriteLine("Неверная скобочная последовательность");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
