ce1941e9898d3f2496cad3e8628db871f744b1c9
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-07-15 20:29:28 +0300

    Add lab17 project

---
+++ Lab17/Task1/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Account<T>
+    {
+        T number;
+        decimal balance;
+        string name;
+
+        public void SetNumber(T number) => this.number = number;
+
+        public T GetNumber() => number;
+
+        public void SetName(string name) => this.name = name;
+        public string GetName() => name;
+
+        public void SetBalance(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Баланс не может быть меньше нуля.");
+            }
+            balance = amount;
+        }
+        public decimal GetBalance() => balance;
+
+        public string GetInfo()
+        {
+            return $"Аккаунт номер {GetNumber()}\nКлиент: {GetName()}\nБаланс: {GetBalance()}";
+    }
+    }
+}
---
+++ Lab17/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<string> account1 = new Account<string>();
+            account1.SetNumber("N1");
+            account1.SetName("Jhon");
+            account1.SetBalance(1000);
+            Console.WriteLine(account1.GetInfo());
+
+            Console.WriteLine();
+
+            Account<int> account2 = new Account<int>();
+            account2.SetNumber(1001);
+            account2.SetName("Alice");
+            account2.SetBalance(99.99m);
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
