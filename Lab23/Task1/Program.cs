24a1ae3c56cb67cc4cc61c0f0c4f2f20ad77f270
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-07-17 20:54:14 +0300

    Add lab23 project

---
+++ Lab23/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        /* Разработать асинхронный метод для вычисления факториала числа.
+         * В методе Main выполнить проверку работы метода.*/
+
+        static void Main(string[] args)
+        {
+
+            FactorialAsync(17);
+            FactorialAsync(5);
+
+            Console.WriteLine("Вычислем факториалы. Для меньшего числа n вывод будет раньше.");
+
+            Console.ReadKey();
+        }
+
+        static async void FactorialAsync(uint n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+
+        static void Factorial(uint n)
+        {
+            ulong result = 1;
+
+            for (uint i = 2; i < n + 1; i++)
+            {
+                result *= i;
+                Thread.Sleep(150);
+            }
+
+            Console.WriteLine($"Факториал n={n} равен {result}");
+        }
+        
+    }
+}
